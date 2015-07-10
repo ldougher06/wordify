ccdb3996f4a3668ee8705dfb641a047d4da96d20
logan dougher <logan.dougher@gmail.com>
2015-07-10 15:12:15 -0500

    api bad search request, basic request needs work

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,38 +1,34 @@
 angular.module('starter.controllers', [])
 
-<<<<<<< HEAD
-.controller('DashCtrl', function($scope) {
-
-})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+.controller('DashCtrl', function($scope, $http) {
+  var getAlbum = function (albumId, callback) {
+    $http({
+        url: 'https://api.spotify.com/v1/albums/' + albumId,
+        success: function (response) {
+            callback(response);
+        }
+    })
   }
-})
+  var searchAlbums = function (query) {
+    $http({
+      url: 'https://api.spotify.com/v1/search',
+      data: {
+        q: query,
+        type: 'album'
+      }
+    })
+  }
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+  document.getElementById('search-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    searchAlbums(document.getElementById('query').value);
+    }, false);
+  })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-=======
+
 .controller('playlistCtrl', function($scope) {
   var API = "https://api.spotify.com/v1/search?q=";
   var list = "";
   list = $scope.songsearch;
   console.log(list);
->>>>>>> 1f929bc6dcfe4525067abccf070b2ec5bb9f318b
 });
